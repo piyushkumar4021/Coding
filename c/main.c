06211072caab842079ea413e480a3364f0d50abb
piyushkumar4021 <133260831+piyushkumar4021@users.noreply.github.com>
2023-05-27 17:24:16 +0000

    Hey There.

--- c/main.c
+++ c/main.c
@@ -1,69 +1,16 @@
-#include<stdio.h>
-#include"limits.h"
-// This File is Under Construction
+#include <stdio.h>
 
-int doUnion(int sizeA, int sizeB, int a[], int b[], int result[]){
-    int i,j,t,count=0;
+int main() {
 
-    for(i=0; i<sizeA; i++){
-        result[i]=a[i];
-    }
-    count=sizeA;
+    int size;
+    scanf("%d",&size);
 
-    for(i=0; i<sizeB; i++){
-        t=1;
-
-        for(j=0; j<sizeA; j++){
-            if(b[i]==a[j]){
-                t=0;
-                break;
-            }
-        }
-
-        if(t==1){
-            result[count]=b[i];
-            count++;
-        }
+    int i,j,arr[size];
+    for(i=0; i<size; i++){
+        scanf("%d",&arr[i]);
     }
 
-    return count;
-}
-
-void sortArray(int a[], int elements){
-    int i,j,temp;
-
-    for(i=0; i<elements; i++){
-        for(j=i+1; j<elements; j++){
-            if(a[i]>a[j]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-}
-
-int main(){
     
-    int sizeA,sizeB;
-    scanf("%d %d",&sizeA,&sizeB);
-
-    int a[sizeA],b[sizeB],i;
-
-    for(i=0; i<sizeA; i++)
-    scanf("%d",&a[i]);
-
-    for(i=0; i<sizeB; i++)
-    scanf("%d",&b[i]);
-
-    int result[sizeA+sizeB],elements;
-    elements=doUnion(sizeA,  sizeB,  a,  b, result);
-
-    sortArray(result, elements);
-
-    for(i=0; i<elements; i++){
-        printf("%d\t",result[i]);
-    }
-
+    
     return 0;
 }
