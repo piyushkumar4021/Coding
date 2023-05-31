a62ebc6845ae181edf413b669c318a7ea7c1aa3d
piyushkumar4021 <133260831+piyushkumar4021@users.noreply.github.com>
2023-05-31 12:54:30 +0000

    hello

--- c/main.c
+++ c/main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
+#include <limits.h>
 
-int main() {
+int find(int arr[], int size, int target)
+{
+    int i;
 
-    int size;
-    scanf("%d",&size);
-
-    int i,j,arr[size];
-    for(i=0; i<size; i++){
-        scanf("%d",&arr[i]);
+    for (i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+            return -1;
     }
 
-    
-    
+    return 0;
+}
+
+int main()
+{
+
+    int size;
+    scanf("%d", &size);
+
+    int i, j, arr[size];
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int target;
+    scanf("%d", &target);
+
+    printf("%d", find(arr, size, target));
+
     return 0;
 }
